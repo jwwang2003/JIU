c8936a177a5e06cb22015e3192288d99b7242ca8
jwwang2003 <jwwang.2003@gmail.com>
2020-10-16 18:01:59 -0700

    Updates

---
+++ precache-manifest.c439c6fd40e33236da21050a72765387.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e988dafe8983435da4566114b556e4b3",
+    "url": "/JIU/index.html"
+  },
+  {
+    "revision": "21e2b276e6d7c75ed2f4",
+    "url": "/JIU/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c1533d3bb0f5dece3b46",
+    "url": "/JIU/static/css/main.cbf23181.chunk.css"
+  },
+  {
+    "revision": "21e2b276e6d7c75ed2f4",
+    "url": "/JIU/static/js/2.f500b7f5.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/JIU/static/js/2.f500b7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1533d3bb0f5dece3b46",
+    "url": "/JIU/static/js/main.7d22608c.chunk.js"
+  },
+  {
+    "revision": "c9a81c4381bf044050d2",
+    "url": "/JIU/static/js/runtime-main.ac2313a8.js"
+  },
+  {
+    "revision": "0867656653e89cc53b75f5bf711d7128",
+    "url": "/JIU/static/media/facebook.08676566.svg"
+  },
+  {
+    "revision": "bfd6437fb0f222767f335e8702953b0d",
+    "url": "/JIU/static/media/google.bfd6437f.svg"
+  },
+  {
+    "revision": "8b055b73958b872be2cfa9b8a123fb08",
+    "url": "/JIU/static/media/logo.8b055b73.png"
+  },
+  {
+    "revision": "1dfdc05ad0dcfb8c8e69d5db384c28c6",
+    "url": "/JIU/static/media/weixin.1dfdc05a.png"
+  }
+]);
